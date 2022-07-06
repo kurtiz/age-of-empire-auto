13140ebd0b86cdce6c21a457f72892b4b8718ef1
Aaron Will Djaba <papiliocurtis@gmail.com>
2022-07-06 13:07:57 +0000

    Created a simple cheat engine :building_construction:

--- cheatcodes.py
+++ cheatcodes.py
@@ -1,46 +1,20 @@
-import pyautogui
+from CheatEngine import CheatEngine as ce
 import keyboard
-import time
+
+
+def addHotKeys():
+    keyboard.add_hotkey("ctrl+f", lambda: ce.foodCheat())
+    keyboard.add_hotkey("ctrl+c", lambda: ce.coinsCheat())
+    keyboard.add_hotkey("ctrl+w", lambda: ce.woodCheat())
+    keyboard.add_hotkey("ctrl+p", lambda: ce.pandaCheat())
 
 
 def main():
-    # print(
-    #     "Cheats:\n"
-    #     "1. 200,000 Food\n"
-    #     "2. 200,000 Wood\n"
-    #     "3. 200,000 Coins\n"
-    #     "4. Laser Bear\n"
-    # )
-
+    addHotKeys()
+    print(keyboard.get_hotkey_name())
     print("Cheat server started!")
     print("Listening to keystroke.......")
-
     keyboard.wait(".")
-    # userInput = input()
-
-
-                #== "1":
-            #print(keyboard.read_key())
-            # time.sleep(5)
-            # print("hello")
-            # pyautogui.press("enter")
-            # pyautogui.write("Medium Rare Please", interval=0.25)
-            # pyautogui.press("enter")
-        # elif userInput == "2":
-        #     time.sleep(5)
-        #     pyautogui.press("enter")
-        #     pyautogui.write("<censored>", interval=0.25)
-        #     pyautogui.press("enter")
-        # elif userInput == "3":
-        #     time.sleep(5)
-        #     pyautogui.press("enter")
-        #     pyautogui.write("Give me liberty or give me coin", interval=0.25)
-        #     pyautogui.press("enter")
-        # elif userInput == "4":
-        #     time.sleep(5)
-        #     pyautogui.press("enter")
-        #     pyautogui.write("o Canada 2005", interval=0.25)
-        #     pyautogui.press("enter")
 
 
 if __name__ == "__main__":
