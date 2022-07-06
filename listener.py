784e6e262ce5d06ba378280478518ed55a461a48
Aaron Will Djaba <papiliocurtis@gmail.com>
2022-07-06 16:13:05 +0000

    Updated README.md with setup and usage instructions :memo:
    Updated README.md and Added requirements.txt

--- listener.py
+++ listener.py
@@ -3,18 +3,23 @@ import keyboard
 
 
 def addHotKeys():
-    keyboard.add_hotkey("ctrl+f", lambda: ce.foodCheat())
-    keyboard.add_hotkey("ctrl+c", lambda: ce.coinsCheat())
-    keyboard.add_hotkey("ctrl+w", lambda: ce.woodCheat())
-    keyboard.add_hotkey("ctrl+p", lambda: ce.pandaCheat())
+    keyboard.add_hotkey("ctrl+[", lambda: ce.foodCheat())
+    keyboard.add_hotkey("ctrl+]", lambda: ce.coinsCheat())
+    keyboard.add_hotkey("ctrl+;", lambda: ce.woodCheat())
+    keyboard.add_hotkey("ctrl+'", lambda: ce.pandaCheat())
 
 
 def main():
     addHotKeys()
-    print(keyboard.get_hotkey_name())
-    print("Cheat server started!")
+
+    print("Cheat server started!\n")
+    print("=========Hot Keys=========")
+    print("Food 100,000 - ctrl f \nWood 100,000 - ctrl w \nCoins 100,000 - ctrl c "
+          "\nLaser Panda - ctrl p \nPress \".\" to shut the listener down\n")
     print("Listening to keystroke.......")
+
     keyboard.wait(".")
+    print("Shutting server down")
 
 
 if __name__ == "__main__":
